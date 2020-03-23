fab48ee7bdd61d53a093d9c8e8c798129f05a74a
cool-library-2371 <est.ariel@gmail.com>
2020-03-23 03:02:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,5 +15,6 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  console.log("I love you,)return  Grandma."
+  console.log("I love you, Grandma.")
+  return string.toLowerCase()
 }
